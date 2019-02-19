9712ef1f6c8697a518306bc6b2ff1b41414cedbc
Dathan Spencer <dpspencer88@gmail.com>
2019-02-19 14:51:59 -0700

    Complete basic functionality

--- client.js
+++ client.js
@@ -1,18 +1,47 @@
 // initializing socket, connection to server
 const socket = io.connect('http://localhost:7777');
-socket.on('connect', function(data) {
+const $messageForm = $('#messageForm');
+const $message = $('#message');
+const $chat = $('#chat');
+const $messageArea = $('#messageArea');
+const $userFormArea = $('#userFormArea');
+const $userForm = $('#userForm');
+const $users = $('#users');
+const $username = $('#username');
+
+socket.on('connect', data => {
 	socket.emit('join', 'Hello server from client');
 });
 
-// listener for 'thread' event, which updates messages
-socket.on('thread', function(data) {
-	$('#thread').append(`<li>${data}</li>`);
+$messageForm.submit(e => {
+	e.preventDefault();
+	socket.emit('send message', $message.val());
+	$message.val('');
 });
 
-// sends message to server, resets & prevents default form action
-$('form').submit(function() {
-	const message = $('#message').val();
-	socket.emit('messages', message);
-	this.reset();
-	return false;
+socket.on('new message', data => {
+	$chat.append(
+		`<div class="card card-body bg-light"><strong>${data.user}</strong> : ${
+			data.msg
+		}</div>`
+	);
+});
+
+$userForm.submit(e => {
+	e.preventDefault();
+	socket.emit('new user', $username.val(), data => {
+		if (data) {
+			$userFormArea.hide();
+			$messageArea.show();
+		}
+	});
+	$username.val('');
+});
+
+socket.on('get users', data => {
+	let html = '';
+	for (let i = 0; i < data.length; i++) {
+		html += `<li class="list-group-item">${data[i]}</li>`;
+	}
+	$users.html(html);
 });
